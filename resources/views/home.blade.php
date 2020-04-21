37592091be25850e48d2ce31a8ee43e29841e6df
marroquin93 <40841652+marroquin93@users.noreply.github.com>
2020-04-20 19:11:35 -0500

    Update home.blade.php
    cambio de home a house

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-12 col-sm-10 col-lg-6 mx-auto">
                 <div class="content-all">
-                    <h1>Estoy en Home</h1>
+                    <h1>Estoy en house</h1>
                 </div>
             </div>
         </div>
