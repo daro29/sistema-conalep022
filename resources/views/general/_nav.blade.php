a8ad473bd378814ae4cf477e7a57cde022fd293f
marroquin93 <40841652+marroquin93@users.noreply.github.com>
2020-05-22 19:51:20 -0500

    agregar fontf-iconos
    agreggue iconos en el home y el nav
    comente una linea de codigo de app @charset "UTF-8";  tiene error

--- resources/views/general/_nav.blade.php
+++ resources/views/general/_nav.blade.php
@@ -3,12 +3,12 @@
 <nav class="navegacion" data-spy="scroll">
     <ul class="menu">
 
-        <li class="title-menu"><span class="fa fa-home icon-menu"></span>Inicio</li>
+        <li class="title-menu"><span class="icon-home1"></span>Inicio</li>
 
         {{-- Importar PTB --}}
         @auth('teacher')
         <li class="item-submenu" menu="1">
-            <a href="#"><span class="fas fa-layer-group icon-menu"></span>Capturar</a>
+            <a href="#"><span class="icon-file-excel-o"></span>Capturar</a>
             <ul class="submenu">
                 <li class="title-menu"><span class="fas fa-graduation-cap icon-menu"></span>Carreras</li>
                 <li class="go-back">Atras</li>
@@ -21,9 +21,9 @@
         </li>
           {{-- Logros --}}
         <li class="item-submenu" menu="3">
-            <a href="#"><span class="fas fa-trophy icon-menu"></span>Logros</a>
+            <a href="#"><span class="icon-line-chart"></span>Logros</a>
             <ul class="submenu">
-                <li class="title-menu"><span class="fas fa-chart-bar icon-menu"></span>Logros</li>
+                <li class="title-menu"><span class="icon-line-chart"></span>Logros</li>
                 <li class="go-back">Atras</li>
                 <li><a href="{{     route('achievements.partial')     }}">Parciales</a></li>
                 <li><a href="{{     route('achievements.biannual')   }}">Semestral</a></li>
@@ -34,9 +34,9 @@
         {{-- Información escolar --}}
         @auth('student')
         <li class="item-submenu" menu="2">
-            <a href="#"><span class="fas fa-user-graduate icon-menu"></span>Información Escolar</a>
+            <a href="#"><span class="icon-book-reference"></span>Información Escolar</a>
             <ul class="submenu">
-                <li class="title-menu"><span class="fas fa-chart-line icon-menu"></span>Información Escolar</li>
+                <li class="title-menu"><span class="icon-book-reference"></span>Información Escolar</li>
                 <li class="go-back">Atras</li>
                 <li><a href="#">Calificaciones Parciales</a></li>
                 <li><a href="#">Boletas</a></li>
@@ -46,10 +46,10 @@
 
         @auth('admin')
 
-        <li><a href="#"><span class="fas fa-file-import icon-menu"></span>Subir Archivo</a></li>
+        <li><a href="#"><span class="icon-archive"></span>Subir Archivo</a></li>
         {{-- Avisos Admin --}}
         <li class="item-submenu" menu="4">
-            <a href="#"><span class="fas fa-envelope icon-menu"></span>Enviar Avisos</a>
+            <a href="#"><span class="icon-send"></span>Enviar Avisos</a>
             <ul class="submenu">
                 <li class="title-menu"><span class="fas fa-envelope-open icon-menu"></span>Avisos</li>
                 <li class="go-back">Atras</li>
@@ -59,9 +59,9 @@
 
         {{-- Subir Archivos Admin --}}
         <li class=" item-submenu" menu="6">
-            <a href="#"><span class="fas fa-file-upload icon-menu"></span>Administrar</a>
+            <a href="#"><span class="icon-folder"></span>Administrar</a>
             <ul class="submenu">
-                <li class="title-menu"><span class="fas fa-file-upload icon-menu"></span>Registro</li>
+                <li class="title-menu"><span class="icon-add-user"></span>Registro</li>
                 <li class="go-back">Atras</li>
                 <li><a href="{{ route('career.index')   }}">Crear Carreras</a></li>
                 <li><a href="{{ route('group.index')    }}">Crear Grupos</a></li>
@@ -73,9 +73,9 @@
 
         @endauth
 
-        <li><a href="{{ route('contact') }}"><span class="fa fa-envelope icon-menu"></span>Contacto</a></li>
+        <li><a href="{{ route('contact') }}"><span class="icon-address-book"></span>Contacto</a></li>
 
-        <a class="dropdown-item fas fa-window-close icon-menu" href="{{ route('logout') }}" onclick="event.preventDefault();
+        <a class="icon-cross" href="{{ route('logout') }}" onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
             <span>Cerrar Sesión</span>
         </a>
